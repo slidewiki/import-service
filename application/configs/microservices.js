42278d2a824163d94645e400058b8123dcf98dde
dpaun <dpaunovic@gmail.com>
2016-09-28 14:18:57 +0200

    SWIK-349 Added fileservice to microservices file

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -5,5 +5,9 @@ module.exports = {
     // uri: 'deckservice.manfredfris.ch'
     uri: 'localhost',
     port: 5000
+  },
+  'file': {
+    url: 'fileservice.manfredfris.ch',
+    shareVolume: '/data/files'
   }
 };
