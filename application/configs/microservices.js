60a0bbdd74cbd6b2bfeb2065af2e2f15c306702b
Luis Fernandes <sednanref2004@gmail.com>
2017-02-18 00:29:44 +0100

    Forgot to change the right version of microservises.js

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -4,10 +4,10 @@ const co = require('../common');
 
 module.exports = {
   'deck': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'https://deckservice'
   },
   'file': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'fileservice',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'https://fileservice',
     shareVolume: '/data/files'
   }
 };
