6fbe1d21330a07652f623d1ff08e7abbb3cd0c9c
dpaun <dpaunovic@gmail.com>
2017-02-23 15:03:44 +0100

    SWIK-1007 Modified microservices uri

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -4,9 +4,9 @@ const co = require('../common');
 
 module.exports = {
   'deck': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'https://deckservice.experimental.slidewiki.org'
   },
   'file': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'http://fileservice'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'https://fileservice.experimental.slidewiki.org'
   }
 };
