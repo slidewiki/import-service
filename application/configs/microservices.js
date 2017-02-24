e13675a496bdf7e1d1d8af7481172c77485364f2
dpaun <dpaunovic@gmail.com>
2017-02-24 10:29:55 +0100

    Reverting uri

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -4,9 +4,10 @@ const co = require('../common');
 
 module.exports = {
   'deck': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'https://deckservice.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
   },
   'file': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'https://fileservice.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'http://fileservice',
+    shareVolume: '/data/files'
   }
 };
