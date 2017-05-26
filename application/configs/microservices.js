6fc4ea6a5054da3efd62b5f8e9afd6085781e2e9
Huw Fryer <huwfryer@gmail.com>
2017-05-26 11:35:39 +0100

    SWIK-1284 Accidentally added dev version of microservices.js. Changing back.

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -4,14 +4,14 @@ const co = require('../common');
 
 module.exports = {
   'deck': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'https://deckservice.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
   },
   'file': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'https://fileservice.experimental.slidewiki.org',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'https://fileservice',
     shareVolume: '/data/files'
   }  ,
   'unoconv': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_UNOCONV)) ? process.env.SERVICE_URL_UNOCONV : 'https://unoconvservice.experimental.slidewiki.org',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_UNOCONV)) ? process.env.SERVICE_URL_UNOCONV : 'http://unoconvservice',
     protocol: 'https:',
     host: (!co.isEmpty(process.env.SERVICE_HOST_UNOCONV)) ? process.env.SERVICE_HOST_UNOCONV : 'unoconvservice',
     path: '/unoconv/pptx',
