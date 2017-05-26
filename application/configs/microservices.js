130a7a9fbaad9d84385b8bbc41f201698ab82478
Huw Fryer <huwfryer@gmail.com>
2017-05-26 11:37:22 +0100

    Change fileservice default URL

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -7,7 +7,7 @@ module.exports = {
     uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
   },
   'file': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'https://fileservice',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_FILE)) ? process.env.SERVICE_URL_FILE : 'http://fileservice',
     shareVolume: '/data/files'
   }  ,
   'unoconv': {
